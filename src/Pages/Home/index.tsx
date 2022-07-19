cb345dfe03269f936044870f3f1ea3763e9196b9
Gui Recordon <guilherme.recordon@gmail.com>
2022-07-19 15:04:04 -0300

    Add basic home styles

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,38 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="minutesTotal">durante</label>
+          <input id="minutesTotal" type="number" />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button>
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}
