da3c34f1df55a23e8a8d1789820c1524f1779fab
Gui Recordon <guilherme.recordon@gmail.com>
2022-07-21 15:43:35 -0300

    Rename pages folder

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import { DefaultLayout } from './Layouts/DefaultLayout'
-import { History } from './Pages/Home/History'
+import { History } from './Pages/History'
 import { Home } from './Pages/Home'
 
 export function Router() {
